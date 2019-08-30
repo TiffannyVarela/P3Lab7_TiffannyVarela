1fb5e2166b5d6f7ca68dbba0af2f49bd59169935
TiffannyVarela <tiffanny_varela529@unitec.edu>
2019-08-30 13:43:38 -0600

    Logger

--- Logger.cpp
+++ Logger.cpp
@@ -10,8 +10,58 @@ using namespace std;
 
 class Logger{
 	protected:
+		string user, nom_archv;
+		int num_log;
+		vector <Log*> vect_logs;
 		
 	public:
+		Logger(){
+		}
+		
+		Logger(string user, string nom_archv, int num_log){
+			this->user=user;
+			this->nom_archv=nom_archv;
+			this->num_log=num_log;
+		}
+		
+		string getUser(){
+			return this->user;
+		}
+		
+		void setUser(string user){
+			this->user.assign(user);
+		}
+		
+		string getNom_archv(){
+			return this->nom_archv;
+		}
+		
+		void setNom_archv(string nom_archv){
+			this->nom_archv.assign(nom_archv);
+		}
+		
+		int getNum_log(){
+			return this->num_log;
+		}
+		
+		void setNum_log(int num_log){
+			this->num_log=num_log;
+		}
+		
+		vector <Log*> getLogs(){
+			return this->vect_logs;
+		}
+		
+		void addLog(Log* l){
+			vect_logs.push_back(l);
+		}
+		
+		void setVectLogs( vector <Log*> vect_logs){
+			this->vect_logs=vect_logs;
+		}
+		
+		~Logger(){
+		}
 };
 
 #endif
