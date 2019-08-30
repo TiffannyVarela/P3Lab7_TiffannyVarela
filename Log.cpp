04bf728efded2158e2a0683fad0bc72d0f672ff6
TiffannyVarela <tiffanny_varela529@unitec.edu>
2019-08-30 13:36:06 -0600

    Log

--- Log.cpp
+++ Log.cpp
@@ -8,8 +8,47 @@ using namespace std;
 
 class Log{
 	protected:
+		string user;
+		string comd;
+		int num_log;
 		
 	public:
+		Log(){
+		}
+		
+		Log(int num_log, string user, string comd){
+			this->num_log=num_log;
+			this->user=user;
+			this->comd=comd;
+		}
+		
+		int getNum_log(){
+			return this->num_log;
+		}
+		
+		void setNum_log(int num_log){
+			this->num_log=num_log;
+		}
+		
+		string getUser(){
+			return this->user;
+		}
+		
+		void setUser(string user){
+			this->user.assign(user);
+		}
+		
+		string getComd(){
+			return this->comd;
+		}
+		
+		void setComd(string comd){
+			this->user.assign(user);
+		}
+		
+		~Log(){
+		}
+		
 };
 
 #endif
